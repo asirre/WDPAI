fb9d0612668ad73d119e653df58ca8864f78cbe6
asirre <62615841+asirre@users.noreply.github.com>
2021-01-20 19:43:44 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const USERNAME = 'rnzhludofkpvjs';
-const PASSWORD = 'd67377de8e1dc0256f8b309bdab822f6dde5cff914bf5988456b77dd0af45c6a';
-const HOST = 'ec2-54-247-94-127.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd86mhje81frrps';
-
